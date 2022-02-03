485775ff2d97159c78d44da1b49c17560232a3d8
Fabio Junior <fabioclash2005@gmail.com>
2022-02-02 21:35:55 -0300

    Fix: Arrumando a parte do video e colocando o botão de whatsapp

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,3 +11,14 @@ function limparRotate() {
     document.querySelector('.osso-img').classList.remove('rotate');
     document.querySelector('.osso-img').classList.remove('rotate-desativado');
 }
+
+const bottomScroll = document.querySelector('.whatsApp-link')
+function decidirBotaoScroll() {
+    if (window.scrollY == 0) {
+        bottomScroll.style.display = 'none';
+    } else {
+        bottomScroll.style.display = 'flex';
+    }
+}
+
+window.addEventListener('scroll', decidirBotaoScroll)
